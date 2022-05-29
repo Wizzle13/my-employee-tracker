494a0fe08a64f5eb1ed26ff2e967442936dea831
CBurton <burton.chris13@gmail.com>
2022-05-29 13:28:21 -0500

    final

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# my-employee-tracker
-build a command-line application from scratch to manage a company's employee database, using Node.js, Inquirer, and MySQL.
+# My Employee Tracker
+  [![License: MIT](https://img.shields.io/badge/License-MIT-lightgrey.svg)](https://opensource.org/licenses/MIT)
+
+  ![Open Issues](https://img.shields.io/github/issues-raw/wizzle13/my-employee-tracker?style=plastic)
+  
+  ## Description
+  This project is to build a simple command line interface to a SQL database to veiw employee, departments and roles
+
+  ### Tools and Languages used
+  ![MySQL](https://img.shields.io/badge/MySQL-005C84?style=plastic&logo=mysql&logoColor=white)
+  ![JavaScript](https://img.shields.io/badge/-JavaScript-F7DF1E?style=plastic&logo=Javascript&logoColor=white)
+  ![Insomnia](https://img.shields.io/badge/Insomnia-4000BF?style=plastic&logo=insomnia&logoColor=white)
+  ![ExpressJS](https://img.shields.io/badge/Express.js-404D59?style=plastic)
+
+  ![npm express](https://img.shields.io/npm/v/express?label=express&style=plastic)
+  ![npm inquirer](https://img.shields.io/npm/v/inquirer?label=inquirer&style=plastic)
+  ![npm mysql2](https://img.shields.io/npm/v/mysql2?label=mysql2&style=plastic)
+
+  
+  
+  ## Screenshots
+  <img src = "./screenshots/screenshot1.png">
+  <img src = "./screenshots/screenshot2.png">
+  <img src = "./screenshots/screenshot3.png">
+
+  
+
+  
+
+  ## Links
+  GitHub Repository: https://github.com/wizzle13/my-employee-tracker
+
+  GitHub URL: https://wizzle13.github.io/store_e-commerce
+
+
+![Made With](https://img.shields.io/badge/Made%20with-Ultimate%20README%20Generator-blue?style=plastic)
+
+  &copy;2020 by Chris Burton
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { prompt } = require("inquirer");
 const db = require('./db/connection');
 const apiRoutes = require('./routes/apiRoutes');
 const PORT = process.env.PORT || 3001;
@@ -49,31 +50,123 @@ const menu = () => {
     if (answer.options === 'View All Employees') {
       getEmployees();
     }
+    if (answer.options === 'Add Employee') {
+      addEmployee();
+    }
     if (answer.options === 'View All Roles') {
       getRoles();
+    }
+    if (answer.options === 'Add Role') {
+      addRole();
+    }
+    if (answer.options === 'View All Departments') {
+      getDepartments();
+    }
+    if (answer.options === 'Add Department') {
+      addDepartment();
+    }
+    if (answer.options === 'Exit') {
+      return;
     }
   })
 }
 
-function getRoles() {
-  const sql = `SELECT role.id, role.title, role.salary, department.name AS department 
-  FROM role
-  LEFT JOIN department
-  ON role.department_id = department.id`;
+function addDepartment() {
+  prompt([
+    {
+      type: "input",
+      name: "newDepartment",
+      message: 'Enter your new department'
+    }
+  ])
+  .then(newDepartmentInput => {
+  const sql = `INSERT INTO department (name)`;
+  // runQuery(sql);
+  menu();
+})
+};
+
+function getDepartments() {
+  const sql = `SELECT * FROM department`;
 
   runQuery(sql);
 };
 
-function getEmployees() {
-  const sql = `SELECT employee.id ,employee.first_name, employee.last_name, role.title, role.salary, department.name AS department
-  FROM employee
-  LEFT JOIN role
-  ON employee.role_id = role.id
-  LEFT JOIN department
-  ON role.department_id = department.id`;
+function getRoles() {
+  const sql = `SELECT role.id, role.title, role.salary, department.name AS department 
+    FROM role
+    LEFT JOIN department
+    ON role.department_id = department.id`;
 
   runQuery(sql);
+};
 
+function addRole() {
+  db.query("SELECT name FROM department ORDER BY id;", (err, rows) => {
+
+  prompt([
+    {
+      type: "input",
+      name: "newRole",
+      message: 'Enter your new Role'
+    },
+    {
+      type: "input",
+      name: "newSalary",
+      message: 'Enter the salary for th new Role'
+    },
+    {
+      type: "list",
+      name: "departmentChoices",
+      message: "What department does this role belong to?",
+      choices: rows
+    },
+  ])
+  .then(newRoleInput => {
+    const sql = `INSERT INTO role (title ,salary, department_id) Values (title, salary, department_id)`;
+    // runQuery(sql);
+    menu();
+  })
+  })  
+};
+
+function addEmployee() {
+  db.query("SELECT title FROM role ORDER BY id;", (err, rows) => {
+  prompt([
+    {
+      type: "input",
+      name: "first_name",
+      message: "Enter employee first name."
+    },
+    {
+      type: "input",
+      name: "last_name",
+      message: "Enter employee last name."
+    },
+    {
+      type: "list",
+      name: "roleChoices",
+      message: "Please select Role",
+      choices: rows
+    },
+  ])
+  .then(newEmployeeInput => {
+    // const sql = `INSERT INTO role (title ,salary, department_id) Values (title, salary, department_id)`;
+    // runQuery(sql);
+    menu();
+  })
+})
+};
+
+function getEmployees() {
+  const sql = `SELECT employee.id ,employee.first_name, employee.last_name, role.title, role.salary, department.name AS department
+    FROM employee
+    LEFT JOIN role
+    ON employee.role_id = role.id
+    LEFT JOIN department
+    ON role.department_id = department.id`;
+
+  runQuery(sql);
 };
 
 function runQuery(sql) {
@@ -82,6 +175,7 @@ function runQuery(sql) {
       console.log(err);
     }
     console.table(rows);
+    menu();
   });
 }
 // Create a function to initialize app
